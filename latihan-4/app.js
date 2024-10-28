d67ff224aebb5bd951f78d4faed3fc281ed96ba5
Ghifari-s <ghifaripooh@gmail.com>
2024-10-28 13:13:43 +0700

    commit

--- latihan-4/app.js
+++ latihan-4/app.js
@@ -1,6 +1,6 @@
 const { tambah, kurang } = require("./mathOperations.js");
-const kali = require("./mathOperations.js");
+const {kali} = require("./mathOperations.js");
 
 console.log(tambah(10, 5));
 console.log(kurang(15, 5));
-kali(5,3)
+console.log(kali(5,3))
